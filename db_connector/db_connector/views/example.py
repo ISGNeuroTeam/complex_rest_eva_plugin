e33a8690f5bec54d5c98116c95d9c07e0ca2d6b0
Artem Kushch <akushch@isgneuro.com>
2022-03-10 21:01:22 +0300

    Fix bug with config

--- db_connector/db_connector/views/example.py
+++ db_connector/db_connector/views/example.py
@@ -6,7 +6,9 @@ from rest.views import APIView
 from rest.response import Response, status
 from rest.permissions import IsAuthenticated, AllowAny
 
-from complex_rest_eva_plugin.settings import ini_config
+# from complex_rest_eva_plugin.settings import ini_config
+from complex_rest_eva_plugin.db_connector.db_connector.settings import ini_config
+
 
 # you can use default logger for plugin
 log = logging.getLogger('complex_rest_eva_plugin')
