43111a55126651e7ddc59a12a9d9713bf238783d
Ilia <isagaidak@isgneuro.com>
2022-03-10 12:41:26 +0300

    db connector added

--- dashboards/dashboards/views/dashboards.py
+++ dashboards/dashboards/views/dashboards.py
@@ -3,6 +3,8 @@ from rest.response import Response, status
 from rest.permissions import IsAuthenticated
 import uuid
 import super_logger
+from complex_rest.plugins.db_connector.utils.db_connector import PostgresConnector
+from django.conf import settings
 import json
 
 
@@ -12,13 +14,14 @@ class DashboardsView(APIView):
     http_method_names = ['get']
     handler_id = str(uuid.uuid4())
     logger = super_logger.getLogger('dashboards')
+    db = PostgresConnector(settings.DB_POOL)
 
     def get(self, request):
         kwargs = {}
-        target_group_id = self.get_argument('id', None)
+        target_group_id = request.GET.get('id', None)
         if target_group_id:
             kwargs['group_id'] = target_group_id
-        names_only = self.get_argument('names_only', None)
+        names_only = request.GET.get('names_only', None)
         if names_only:
             kwargs['names_only'] = names_only
         dashs = self.db.get_dashs_data(**kwargs)
@@ -26,20 +29,3 @@ class DashboardsView(APIView):
             {'data': dashs},
             status.HTTP_200_OK
         )
-
-class DashboardsHandler(BaseHandler):
-    async def get(self):
-        kwargs = {}
-
-        if 'list_dashs' in self.permissions or 'admin_all' in self.permissions:
-            target_group_id = self.get_argument('id', None)
-            if target_group_id:
-                kwargs['group_id'] = target_group_id
-            names_only = self.get_argument('names_only', None)
-            if names_only:
-                kwargs['names_only'] = names_only
-        else:
-            raise tornado.web.HTTPError(403, "no permission for list dashs")
-
-        dashs = self.db.get_dashs_data(**kwargs)
-        self.write({'data': dashs})
