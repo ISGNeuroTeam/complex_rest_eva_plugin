041e8d110dcc39eb6ae7bce0e057de58d3ff9543
Artem Kushch <akushch@isgneuro.com>
2022-03-16 19:10:35 +0300

    Update url paths

--- quizs/quizs/urls.py
+++ quizs/quizs/urls.py
@@ -1,35 +1,25 @@
-from rest.urls import path
-from cache import cache_page
-from .views.example import ExampleView
+from django.urls import re_path
 from .views.hello import HelloView
 from .views.quizs import (QuizsHandlerView, QuizHandlerView, QuizFilledHandlerView, QuizQuestionsHandlerView,
                           QuizExportJsonHandlerView, QuizImportJsonHandlerView, CatalogsListHandlerView,
                           CatalogHandlerView, FilledQuizExportHandlerView)
 
-
-# Use cache_page decorator for caching view
-
-# urlpatterns = [
-#     path('example/', cache_page(60 * 15)(ExampleView.as_view())),
-# ]
-
 urlpatterns = [
-path('example/', ExampleView.as_view()),
-path('hello/', HelloView.as_view()),
-path('qapi/quizs/', QuizsHandlerView.as_view()),
-path('qapi/quiz/', QuizHandlerView.as_view()),
-path('qapi/quiz/create/', QuizHandlerView.as_view()),
-path('qapi/quiz/edit/', QuizHandlerView.as_view()),
-path('qapi/quiz/delete/', QuizHandlerView.as_view()),
-path('qapi/quiz/filled/', QuizFilledHandlerView.as_view()),
-path('qapi/quiz/filled/save/', QuizFilledHandlerView.as_view()),
-path('qapi/quiz/questions/', QuizQuestionsHandlerView.as_view()),
-path('qapi/quiz/export/', QuizExportJsonHandlerView.as_view()),
-path('qapi/quiz/import/', QuizImportJsonHandlerView.as_view()),
-path('qapi/catalogs/', CatalogsListHandlerView.as_view()),
-path('qapi/catalog/', CatalogHandlerView.as_view()),
-path('qapi/catalog/create/', CatalogHandlerView.as_view()),
-path('qapi/catalog/edit/', CatalogHandlerView.as_view()),
-path('qapi/catalog/delete/', CatalogHandlerView.as_view()),
-path('qapi/quiz/filled/export/', FilledQuizExportHandlerView.as_view()),
+    re_path('hello/', HelloView.as_view()),
+    re_path(r'^qapi/quizs/?$', QuizsHandlerView.as_view()),
+    re_path(r'^qapi/quiz/?$', QuizHandlerView.as_view()),
+    re_path(r'^qapi/quiz/create/?$', QuizHandlerView.as_view()),
+    re_path(r'^qapi/quiz/edit/?$', QuizHandlerView.as_view()),
+    re_path(r'^qapi/quiz/delete/?$', QuizHandlerView.as_view()),
+    re_path(r'^qapi/quiz/filled/?$', QuizFilledHandlerView.as_view()),
+    re_path(r'^qapi/quiz/filled/save/?$', QuizFilledHandlerView.as_view()),
+    re_path(r'^qapi/quiz/questions/?$', QuizQuestionsHandlerView.as_view()),
+    re_path(r'^qapi/quiz/export/?$', QuizExportJsonHandlerView.as_view()),
+    re_path(r'^qapi/quiz/import/?$', QuizImportJsonHandlerView.as_view()),
+    re_path(r'^qapi/catalogs/?$', CatalogsListHandlerView.as_view()),
+    re_path(r'^qapi/catalog/?$', CatalogHandlerView.as_view()),
+    re_path(r'^qapi/catalog/create/?$', CatalogHandlerView.as_view()),
+    re_path(r'^qapi/catalog/edit/?$', CatalogHandlerView.as_view()),
+    re_path(r'^qapi/catalog/delete/?$', CatalogHandlerView.as_view()),
+    re_path(r'^qapi/quiz/filled/export/?$', FilledQuizExportHandlerView.as_view()),
 ]
